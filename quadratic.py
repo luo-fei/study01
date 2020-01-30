3d1e36d4270161b05c7ca9a360de434d4df77258
luo-fei <fly.luo@gmail.com>
2020-01-30 10:36:33 +0800

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -5,7 +5,7 @@ import math
 
 
 def multi_isinstance ( a, b, c ) :
-    z = [False for i in (a, b, c) if not isinstance ( i, (int))]
+    z = [False for i in (a, b, c) if not isinstance ( i, (int, float))]
     if z :
         return False
     else :
@@ -19,10 +19,13 @@ def quadratic(a, b, c):
     if a == 0:
         x1 = -(c / b)
         x2 = x1
+    elif (b * b - 4 * a * c) < 0:
+        return None
     else:
         x1 = (-b + math.sqrt(b * b - 4 * a * c)) / (2 * a)
         x2 = (-b - math.sqrt(b * b - 4 * a * c)) / (2 * a)
     return x1, x2
 
 
-print(quadratic(2, 5, 3))
+print(quadratic(25, 5, 3))
+
